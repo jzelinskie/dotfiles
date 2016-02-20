8c299751f32caa5f93c7320360ce6db730bc9363
Jimmy Zelinskie <jimmyzelinskie@gmail.com>
2016-02-20 17:02:53 -0500

    install: rm vundle steps

--- install.zsh
+++ install.zsh
@@ -3,5 +3,3 @@ for dotfile in "${ZDOTDIR:-$HOME}"/.dotfiles/^(install.zsh|README.md)(.N); do
   ln -s "$dotfile" "${ZDOTDIR:-$HOME}/.${dotfile:t}"
 done
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-git clone git@github.com:gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
