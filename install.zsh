2c58ac64ba66366e5b5f188809fb981c1c6e5211
Jimmy Zelinskie <jimmy.zelinskie+git@gmail.com>
2017-10-19 22:58:32 -0400

    install.zsh: overhaul & include vim logic

--- install.zsh
+++ install.zsh
@@ -1,9 +1,52 @@
 #!/bin/zsh
 
+set -e
 setopt EXTENDED_GLOB
 
-for dotfile in "${ZDOTDIR:-$HOME}"/.dotfiles/(.??*)(.N); do
-  if [[ ! -a "${ZDOTDIR:-$HOME}/${dotfile:t}" ]]; then
-    ln -s "$dotfile" "${ZDOTDIR:-$HOME}/${dotfile:t}"
+# Default values for configurable variables
+export INSTALL_DIR=${INSTALL_DIR:-$HOME}
+export DOTFILES_DIR=${DOTFILES_DIR:-$HOME/.dotfiles}
+export XDG_HOME=${XDG_HOME:-$INSTALL_DIR/.config}
+
+# Prompt the user
+echo "Installing dotfiles in $DOTFILES_DIR into $INSTALL_DIR"
+echo "You can override these with \$DOTFILES_DIR and \$INSTALL_DIR, respectively"
+echo -n "Continue? "
+read REPLY
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  echo "Cowardly refusing to link dotfiles"
+  exit 1
+fi
+
+# Handle vim first, b/c we need to do extra work to support both vim & neovim.
+echo
+echo "Linking nvim/vim compatible files..."
+mkdir -p $XDG_HOME/nvim
+local VIM_LINKS=(
+  "$DOTFILES_DIR/.vimrc:$XDG_HOME/nvim/init.vim"
+  "$XDG_HOME/nvim/init.vim:$INSTALL_DIR/.vimrc"
+  "$XDG_HOME/nvim:$INSTALL_DIR/.vim"
+)
+for LINK in $VIM_LINKS; do
+  local FILE=`echo $LINK | awk -F":" '{print $1}'`
+  local TARGET=`echo $LINK | awk -F":" '{print $2}'`
+  if [[ ! -a $TARGET ]]; then
+    print -P -- "  %F{002}Linking file:%f $FILE => $TARGET"
+    ln -s $FILE $TARGET
+  else
+    print -P -- "  %F{011}Link already exists:%f $FILE => $TARGET"
+  fi
+done
+
+# Symlink the rest of the dotfiles
+echo
+echo "Linking the rest of the dotfiles..."
+for FILE in $DOTFILES_DIR/(.??*)(.N); do
+  local TARGET=$INSTALL_DIR/${FILE:t}
+  if [[ ! -a $INSTALL_DIR/${FILE:t} ]]; then
+    print -P -- "  %F{002}Linking file:%f $FILE => $TARGET"
+    ln -s $FILE $INSTALL_DIR/${FILE:t}
+  else
+    print -P -- "  %F{011}Link already exists:%f $FILE => $TARGET"
   fi
 done
