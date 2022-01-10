4f7a3987c9add503f7765a7de605141c0cfe8d3f
Jimmy Zelinskie <jimmy@zelinskie.com>
2022-01-09 20:49:36 -0500

    vim: organize config under plugin install

--- init.lua
+++ init.lua
@@ -2,14 +2,14 @@
 vim.g.mapleader = ','
 
 -- mapping functions
-local nmap        = function(lhs, rhs) vim.api.nvim_set_keymap('n', lhs, rhs, {}) end
-local vmap        = function(lhs, rhs) vim.api.nvim_set_keymap('v', lhs, rhs, {}) end
-local xmap        = function(lhs, rhs) vim.api.nvim_set_keymap('v', lhs, rhs, {}) end
-local snmap       = function(lhs, rhs) vim.api.nvim_set_keymap('n', lhs, rhs, { silent = true}) end
-local nnoremap    = function(lhs, rhs) vim.api.nvim_set_keymap('n', lhs, rhs, { noremap = true }) end
-local inoremap    = function(lhs, rhs) vim.api.nvim_set_keymap('i', lhs, rhs, { noremap = true }) end
-local bufsnoremap = function(lhs, rhs) vim.api.nvim_buf_set_keymap(0, 'n', lhs, rhs, { noremap = true, silent = true }) end
-local lspremap    = function(keymap, fn_name) bufsnoremap(keymap, '<cmd>lua vim.lsp.' .. fn_name .. '()<CR>') end
+function nmap(lhs, rhs) vim.api.nvim_set_keymap('n', lhs, rhs, {}) end
+function vmap(lhs, rhs) vim.api.nvim_set_keymap('v', lhs, rhs, {}) end
+function xmap(lhs, rhs) vim.api.nvim_set_keymap('v', lhs, rhs, {}) end
+function snmap(lhs, rhs) vim.api.nvim_set_keymap('n', lhs, rhs, { silent = true}) end
+function nnoremap(lhs, rhs) vim.api.nvim_set_keymap('n', lhs, rhs, { noremap = true }) end
+function inoremap(lhs, rhs) vim.api.nvim_set_keymap('i', lhs, rhs, { noremap = true }) end
+function bufsnoremap(lhs, rhs) vim.api.nvim_buf_set_keymap(0, 'n', lhs, rhs, { noremap = true, silent = true }) end
+function lspremap(keymap, fn_name) bufsnoremap(keymap, '<cmd>lua vim.lsp.' .. fn_name .. '()<CR>') end
 
 -- Define autocommands in lua
 -- https://github.com/neovim/neovim/pull/12378
@@ -25,42 +25,6 @@ function nvim_create_augroups(definitions)
     vim.api.nvim_command('augroup END')
   end
 end
-
--- bootstrap packer
-local packer_exists = pcall(vim.cmd, [[packadd packer.nvim]])
-if not packer_exists then
-  local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
-  vim.fn.system('git clone --depth 1 https://github.com/wbthomason/packer.nvim ' .. install_path)
-  vim.cmd [[packadd packer.nvim]]
-end
-
--- packer
-local packer = require('packer').startup {
-  function(use)
-    use {'bogado/file-line'}
-    use {'ervandew/supertab'}
-    use {'fatih/vim-go'}
-    use {'folke/which-key.nvim'}
-    use {'jjo/vim-cue'}
-    use {'junegunn/vim-easy-align'}
-    use {'jzelinskie/monokai-soda.vim', requires = 'tjdevries/colorbuddy.vim'}
-    use {'majutsushi/tagbar'}
-    use {'milkypostman/vim-togglelist'}
-    use {'neovim/nvim-lspconfig'}
-    use {'norcalli/nvim-colorizer.lua'}
-    use {'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}}
-    use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
-    use {'ray-x/lsp_signature.nvim'}
-    use {'tpope/vim-commentary'}
-    use {'tpope/vim-repeat'}
-    use {'tpope/vim-surround'}
-    use {'tpope/vim-unimpaired'}
-    use {'vim-airline/vim-airline-themes', requires = 'vim-airline/vim-airline'}
-    use {'vim-scripts/a.vim'}
-    use {'wbthomason/packer.nvim', opt = true}
-  end,
-}
-if not packer_exists then packer.sync() end -- install on first run
 
 -- misc global opts
 local settings = {
@@ -99,116 +63,14 @@ local settings = {
   'set wrapscan',
   'set termguicolors',
   'set cpoptions+=_',
-  'colorscheme monokai-soda',
 }
 for _, setting in ipairs(settings) do vim.cmd(setting) end
-
--- setup colorizer
-require('colorizer').setup()
-
--- setup treesitter
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = "maintained",
-  highlight = { enable = true },
-}
-
--- vim-go
-vim.g.go_echo_go_info = 0 -- https://github.com/fatih/vim-go/issues/2904#issuecomment-637102187
-vim.g.go_fmt_command = 'gopls'
-vim.g.go_gopls_gofumpt = 1
-
--- language server
-local lspcfg = {
-  gopls =            {binary = 'gopls',                    format_on_save = nil         },
-  golangci_lint_ls = {binary = 'golangci-lint-langserver', format_on_save = nil         },
-  pylsp =            {binary = 'pylsp',                    format_on_save = '*.py'      },
-  pyright =          {binary = 'pyright',                  format_on_save = nil         },
-  rust_analyzer =    {binary = 'rust-analyzer',            format_on_save = '*.rs'      },
-  clojure_lsp =      {binary = 'clojure-lsp',              format_on_save = '*.clj'     },
-  yamlls =           {binary = 'yamlls',                   format_on_save = nil         },
-  bashls =           {binary = 'bash-language-server',     format_on_save = nil         },
-  dockerls =         {binary = 'docker-langserver',        format_on_save = 'Dockerfile'},
-}
-local lsp_keymaps = {
-  {capability = 'declaration',      mapping = 'gd',    command = 'buf.declaration'     },
-  {capability = 'implementation',   mapping = 'gD',    command = 'buf.implementation'  },
-  {capability = 'goto_definition',  mapping = '<c-]>', command = 'buf.definition'      },
-  {capability = 'type_definition',  mapping = '1gD',   command = 'buf.type_definition' },
-  {capability = 'hover',            mapping = 'K',     command = 'buf.hover'           },
-  {capability = 'signature_help',   mapping = '<c-k>', command = 'buf.signature_help'  },
-  {capability = 'find_references',  mapping = 'gr',    command = 'buf.references'      },
-  {capability = 'document_symbol',  mapping = 'g0',    command = 'buf.document_symbol' },
-  {capability = 'workspace_symbol', mapping = 'gW',    command = 'buf.workspace_symbol'},
-}
-local custom_lsp_attach = function(client)
-  require('lsp_signature').on_attach { hint_enable = false }
-  local opts = lspcfg[client.name]
-
-  -- autocomplete
-  vim.api.nvim_buf_set_option(0, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- format on save
-  if opts['format_on_save'] ~= nil then
-    nvim_create_augroups({[client.name] = {{'BufWritePre', opts['format_on_save'], ':lua vim.lsp.buf.formatting_sync(nil, 1000)'}}})
-  end
-
-  -- conditional keymaps
-  for _, keymap in ipairs(lsp_keymaps) do
-    if client.resolved_capabilities[keymap.capability] then
-      lspremap(keymap.mapping, keymap.command)
-    end
-  end
-
-  -- unconditional keymaps
-  lspremap('gl', 'diagnostic.show_line_diagnostics')
-end
-
--- only setup lsp clients for binaries that exist
-local lsp = require('lspconfig')
-for srv, opts in pairs(lspcfg) do
-  if vim.fn.executable(opts['binary']) then lsp[srv].setup({ on_attach = custom_lsp_attach }) end
-end
-
--- productive arrow keys
-nmap('<Up>',    '[e')
-vmap('<Up>',    '[egv')
-nmap('<Down>',  ']e')
-vmap('<Down>',  ']egv')
-nmap('<Left>',  '<<')
-nmap('<Right>', '>>')
-vmap('<Left>',  '<gv')
-vmap('<Right>', '>gv')
 
 -- clear hlsearch on redraw
 nnoremap('<C-L>', ':nohlsearch<CR><C-L>')
 
--- easy align
-xmap('ga', '<Plug>(EasyAlign)')
-nmap('ga', '<Plug>(EasyAlign)')
-
--- telescope
-nnoremap('<c-p>', "<cmd>lua require('telescope.builtin').find_files()<cr>")
-nnoremap('<c-b>', "<cmd>lua require('telescope.builtin').buffers()<cr>")
-nnoremap('<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<cr>")
-nnoremap('<leader>fg', "<cmd>lua require('telescope.builtin').live_grep()<cr>")
-nnoremap('<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<cr>")
-nnoremap('<leader>fh', "<cmd>lua require('telescope.builtin').help_tags()<cr>")
-
--- supertab
-vim.g.SuperTabDefaultCompletionType = "<c-x><c-o>"
-inoremap('<S-Tab>', '<C-v><Tab>')
-
 -- clipboard
 if vim.fn.has('unnamedplus') then vim.o.clipboard = 'unnamedplus' else vim.o.clipboard = 'unnamed' end
-
--- airline
-vim.g.airline_theme = 'monochrome'
-vim.g.airline_powerline_fonts = '0'
-vim.cmd('let g:airline#extensions#tabline#enabled = 1')
-vim.cmd('let g:airline#extensions#tabline#buffer_nr_show = 1')
-
--- tags
-snmap('<leader>o', ':TagbarToggle<CR>')
 
 -- toggle paste and wrap
 snmap('<leader>p', ':set invpaste<CR>:set paste?<CR>')
@@ -216,3 +78,174 @@ snmap('<leader>w', ':set invwrap<CR>:set wrap?<CR>')
 
 -- strip trailing whitespace
 nnoremap('<leader>sws', ':%s/\\s\\+$//e<CR>')
+
+-- bootstrap packer
+function exclude_on_bootstrap(fn) if not packer_bootstrap then return fn end end
+local packer_install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+if vim.fn.empty(vim.fn.glob(packer_install_path)) > 0 then
+  packer_bootstrap = true
+  vim.fn.system('git clone --depth 1 https://github.com/wbthomason/packer.nvim ' .. packer_install_path)
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- packer
+return require('packer').startup(function(use)
+  use {
+    'bogado/file-line',
+    'folke/which-key.nvim',
+    'jjo/vim-cue',
+    'milkypostman/vim-togglelist',
+    'ray-x/lsp_signature.nvim',
+    'tpope/vim-commentary',
+    'tpope/vim-repeat',
+    'tpope/vim-surround',
+    'vim-scripts/a.vim',
+    'wbthomason/packer.nvim',
+
+    {
+      'tpope/vim-unimpaired',
+      config = exclude_on_bootstrap(function()
+        nmap('<Up>',    '[e')
+        vmap('<Up>',    '[egv')
+        nmap('<Down>',  ']e')
+        vmap('<Down>',  ']egv')
+        nmap('<Left>',  '<<')
+        nmap('<Right>', '>>')
+        vmap('<Left>',  '<gv')
+        vmap('<Right>', '>gv')
+      end),
+    },
+    {
+      'jzelinskie/monokai-soda.vim',
+      requires = 'tjdevries/colorbuddy.vim',
+      config = exclude_on_bootstrap(function()
+        vim.cmd('colorscheme monokai-soda')
+      end),
+    },
+    {
+      'norcalli/nvim-colorizer.lua',
+      config = exclude_on_bootstrap(function()
+        require('colorizer').setup()
+      end),
+    },
+    {
+      'fatih/vim-go',
+      config = exclude_on_bootstrap(function()
+        vim.g.go_echo_go_info = 0 -- https://github.com/fatih/vim-go/issues/2904#issuecomment-637102187
+        vim.g.go_fmt_command = 'gopls'
+        vim.g.go_gopls_gofumpt = 1
+      end),
+    },
+    {
+      'junegunn/vim-easy-align',
+      config = exclude_on_bootstrap(function()
+        xmap('ga', '<Plug>(EasyAlign)')
+        nmap('ga', '<Plug>(EasyAlign)')
+      end),
+    },
+    {
+      'nvim-treesitter/nvim-treesitter',
+      config = exclude_on_bootstrap(function()
+        require('nvim-treesitter.configs').setup({
+          ensure_installed = "maintained",
+          highlight = { enable = true },
+        })
+      end),
+    },
+    {
+      'vim-airline/vim-airline-themes',
+      requires = 'vim-airline/vim-airline',
+      config = exclude_on_bootstrap(function()
+        vim.g.airline_theme = 'monochrome'
+        vim.g.airline_powerline_fonts = '0'
+        vim.cmd('let g:airline#extensions#tabline#enabled = 1')
+        vim.cmd('let g:airline#extensions#tabline#buffer_nr_show = 1')
+      end),
+    },
+    {
+      'ervandew/supertab',
+      config = exclude_on_bootstrap(function()
+        inoremap('<S-Tab>', '<C-v><Tab>')
+        vim.g.SuperTabDefaultCompletionType = "<c-x><c-o>"
+      end),
+    },
+    {
+      'majutsushi/tagbar',
+      config = exclude_on_bootstrap(function()
+        snmap('<leader>o', ':TagbarToggle<CR>')
+      end),
+    },
+    {
+      'nvim-telescope/telescope.nvim',
+      requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}},
+      config = exclude_on_bootstrap(function()
+        nnoremap('<c-p>', "<cmd>lua require('telescope.builtin').find_files()<cr>")
+        nnoremap('<c-b>', "<cmd>lua require('telescope.builtin').buffers()<cr>")
+        nnoremap('<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<cr>")
+        nnoremap('<leader>fg', "<cmd>lua require('telescope.builtin').live_grep()<cr>")
+        nnoremap('<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<cr>")
+        nnoremap('<leader>fh', "<cmd>lua require('telescope.builtin').help_tags()<cr>")
+      end),
+    },
+    {
+      'neovim/nvim-lspconfig',
+      config = exclude_on_bootstrap(function()
+        local lspcfg = {
+          gopls =            {binary = 'gopls',                    format_on_save = nil         },
+          golangci_lint_ls = {binary = 'golangci-lint-langserver', format_on_save = nil         },
+          pylsp =            {binary = 'pylsp',                    format_on_save = '*.py'      },
+          pyright =          {binary = 'pyright',                  format_on_save = nil         },
+          rust_analyzer =    {binary = 'rust-analyzer',            format_on_save = '*.rs'      },
+          clojure_lsp =      {binary = 'clojure-lsp',              format_on_save = '*.clj'     },
+          yamlls =           {binary = 'yamlls',                   format_on_save = nil         },
+          bashls =           {binary = 'bash-language-server',     format_on_save = nil         },
+          dockerls =         {binary = 'docker-langserver',        format_on_save = 'Dockerfile'},
+        }
+        local lsp_keymaps = {
+          {capability = 'declaration',      mapping = 'gd',    command = 'buf.declaration'     },
+          {capability = 'implementation',   mapping = 'gD',    command = 'buf.implementation'  },
+          {capability = 'goto_definition',  mapping = '<c-]>', command = 'buf.definition'      },
+          {capability = 'type_definition',  mapping = '1gD',   command = 'buf.type_definition' },
+          {capability = 'hover',            mapping = 'K',     command = 'buf.hover'           },
+          {capability = 'signature_help',   mapping = '<c-k>', command = 'buf.signature_help'  },
+          {capability = 'find_references',  mapping = 'gr',    command = 'buf.references'      },
+          {capability = 'document_symbol',  mapping = 'g0',    command = 'buf.document_symbol' },
+          {capability = 'workspace_symbol', mapping = 'gW',    command = 'buf.workspace_symbol'},
+        }
+        local custom_lsp_attach = function(client)
+          require('lsp_signature').on_attach { hint_enable = false }
+          local opts = lspcfg[client.name]
+
+          -- autocomplete
+          vim.api.nvim_buf_set_option(0, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+          -- format on save
+          if opts['format_on_save'] ~= nil then
+            nvim_create_augroups({[client.name] = {{'BufWritePre', opts['format_on_save'], ':lua vim.lsp.buf.formatting_sync(nil, 1000)'}}})
+          end
+
+          -- conditional keymaps
+          for _, keymap in ipairs(lsp_keymaps) do
+            if client.resolved_capabilities[keymap.capability] then
+              lspremap(keymap.mapping, keymap.command)
+            end
+          end
+
+          -- unconditional keymaps
+          lspremap('gl', 'diagnostic.show_line_diagnostics')
+        end
+
+        -- only setup lsp clients for binaries that exist
+        local lsp = require('lspconfig')
+        for srv, opts in pairs(lspcfg) do
+          if vim.fn.executable(opts['binary']) then lsp[srv].setup({ on_attach = custom_lsp_attach }) end
+        end
+      end),
+    },
+  }
+
+  -- bootstrap installation on first run
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
